e01b28dc89aedc3a3ec92dc8540760d85d1c067a
MacloMen336 <128580516+MacloMen336@users.noreply.github.com>
2023-04-18 21:31:46 +0800

    Update First.py

--- SP_lab_1/First.py
+++ SP_lab_1/First.py
@@ -1,8 +1,14 @@
 import socket #socket for ip and pc login
 import getpass #getpass for username
-Ip = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #take ip and socket
-Ip.connect(("google.ru",80)) #test connect
-Ip_for_print= Ip.getsockname()
 print ("Hello ",getpass.getuser()) #take username
 print ("Your login:",socket.gethostname()) #take login
-print ("Your IP:",Ip_for_print[0]) #vivod Ip adressa
+print ("Your External IP:",socket.gethostbyname_ex(socket.gethostname())[-1][0]) #take external IP
+#-- Old version (with connect website)--#
+# import socket #socket for ip and pc login
+# import getpass #getpass for username
+# Ip = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #take ip and socket
+# Ip.connect(("google.ru",80)) #test connect
+# Ip_for_print= Ip.getsockname()
+# print ("Hello ",getpass.getuser()) #take username
+# print ("Your login:",socket.gethostname()) #take login
+# print ("Your IP:",Ip_for_print[0]) #vivod Ip adressa
