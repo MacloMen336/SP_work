e84a4677a65a82f8bcf3fb9246c391cd7d23fc07
MacloMen336 <128580516+MacloMen336@users.noreply.github.com>
2023-04-18 14:55:51 +0800

    Delete SP_lab_1 directory

--- SP_lab_1/First.py
+++
@@ -1,8 +0,0 @@
-import socket #socket for ip and pc login
-import getpass #getpass for username
-Ip = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #take ip and socket
-Ip.connect(("google.ru",80)) #test connect
-Ip_for_print= Ip.getsockname()
-print ("Hello ",getpass.getuser()) #take username
-print ("Your login:",socket.gethostname()) #take login
-print ("Your IP:",Ip_for_print[0]) #vivod Ip adressa
