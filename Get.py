d49a58f065cd6ae2bec9b368a0ce859c2426e5c6
MacloMen336 <128580516+MacloMen336@users.noreply.github.com>
2023-04-18 14:54:55 +0800

    Add files via upload

---
+++ Get.py
@@ -0,0 +1,64 @@
+from bs4 import BeautifulSoup
+import requests
+from Web_site import settings
+
+import re
+
+print('Расписание группы И-101 или Всех групп на день? вывести 1 или 2?')
+what_need = int(input())
+if what_need == 1:
+    site_url = settings['rasp_i_101']
+    num_delete_line = 104
+elif what_need == 2:
+    site_url = settings['rasp_all_grups']
+    num_delete_line = 98
+else:
+    site_url = settings['rasp_i_101']
+    num_delete_line = 104
+url = site_url #website 
+resp = requests.get(url) #get all from site ()
+resp.encoding = 'windows-1251' #because site biik.ru rasp use cp-1251 need do that 
+print(resp) #write site answer code
+if resp == 404: #check if site not work
+    print('Not Found')
+else:
+    all_web_code = resp.text #take text chapter from site 
+    bs = BeautifulSoup(all_web_code,"html.parser") #bs html.parser needed site 
+    Lesson = bs.findAll('td') #find all class in tag 'td'
+
+    file = open("Rasp.txt", "w") #write in file
+    for item in Lesson: #write all text from html tag in txt file
+        file.write(item.text +"\n")
+    file.close()
+    
+    line = open("Rasp.txt").readlines() #list all line in txt file 
+    for i in range(num_delete_line): #Delete first 104 line, because they unused
+        line.pop(0)
+
+    f = open("Rasp.txt", "w") #after delete unused elements, write new line in txt file
+    for i in line:
+        f.write(i)
+    f.close()
+
+    # форма таблицы вывода
+    # for i in range(len(line)):
+
+    #     if '\d{2}.\d{2}.' in line[i]:
+    #         print('A')
+    #         line[i] = line[i] + ' Подгруппа 1       Подгруппа 2'
+
+    # print('Schedule on next study day: ') #write in console schedule on next study day
+    # f = open("Rasp.txt", 'r')
+    # f1 = open("Rasp_next_day.txt", "w")
+    # for i in range(19):
+    #     strok = f.readline()
+    #     f1.write(strok)
+    #     print(strok[:-1]) #delete last symbol (/n) from line
+    # f.close()
+
+
+    
+    print('All Ready') #write if programm do all work
+    #print(settings['rasp_i_101'])
+    print(line)
+    
---
+++ Web_site.py
@@ -0,0 +1,5 @@
+settings = {
+'rasp_i_101': 'http://biik.ru/rasp/cg109.htm',
+'rasp_all_grups': 'http://biik.ru/rasp/hg.htm'
+
+}
